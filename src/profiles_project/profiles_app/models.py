3e49a49b9cf1296039fc1f9e5aa909aa6e8d79bc
Mohamed_Sawy <mohamedsawy1022@gmail.com>
2023-02-11 13:05:40 +0200

    created user model manager and configured settings to use the custom user model

--- src/profiles_project/profiles_app/models.py
+++ src/profiles_project/profiles_app/models.py
@@ -5,7 +5,29 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    ...
+    """Helps Django work with the UserProfile custom model"""
+
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('You must enter a valid email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.model(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
