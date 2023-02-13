48c8f166830034c55402e229a25f1fa0571f09fd
Mohamed_Sawy <mohamedsawy1022@gmail.com>
2023-02-13 22:42:04 +0200

    created a simple view with GET method to test the api

--- src/profiles_project/profiles_app/views.py
+++ src/profiles_project/profiles_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Testing the api view"""
+
+    def get(self, request, format=None):
+        """Returns a list of strings for testing"""
+
+        api_list = [
+            'this is just',
+            'for testing',
+            'the APIView'
+        ]
+
+        return Response({'message': 'Hello', 'items': api_list})
