b192d1724157d50ffafb54b83d7df7a0b8f78ef2
Mohamed_Sawy <mohamedsawy1022@gmail.com>
2023-02-14 17:07:42 +0200

    created a serialzer and a POST method for testing

--- src/profiles_project/profiles_app/views.py
+++ src/profiles_project/profiles_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 class HelloApiView(APIView):
     """Testing the api view"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of strings for testing"""
@@ -16,3 +20,15 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'items': api_list})
+
+    def post(self, request):
+        """create a hello message with the name requested for testing"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        name = serializer.data.get('name')
+        message = f'Hello {name}'
+        return Response({'message': message})
